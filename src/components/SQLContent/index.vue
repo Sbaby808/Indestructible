e8de42368d2eb0d785bc2201f586917725930a33
Sbaby <corwin808@gmail.com>
2020-02-22 00:45:09 +0800

    add CodeMirror SQL editor

--- src/components/SQLContent/index.vue
+++ src/components/SQLContent/index.vue
@@ -1,24 +1,63 @@
 <template>
-	<div>
-    SQL<br />
-    SQL<br />
-    SQL<br />
-    SQL<br />
-    lalal
-	</div>
+	<div class="edit-code-editor" style="text-align: left;">
+    <textarea placeholder="按Ctrl键进行代码提示" ref="mycode" class="codesql" v-model="code" style="height:200px;width:auto;"></textarea>
+  </div>
 </template>
 
 <script>
+  import global_varibles from '../global_varibles';
+  import "codemirror/theme/mdn-like.css";
+  import "codemirror/lib/codemirror.css";
+  import "codemirror/addon/hint/show-hint.css";
+
+  let CodeMirror = require("codemirror/lib/codemirror");
+  require("codemirror/addon/edit/matchbrackets");
+  require("codemirror/addon/selection/active-line");
+  require("codemirror/mode/sql/sql");
+  require("codemirror/addon/hint/show-hint");
+  require("codemirror/addon/hint/sql-hint");
 	export default {
 		name:'SQLContent',
 		data() {
 			return {
-
+        code: ''
 			};
-		}
+		},
+    mounted () {
+        debugger
+        let mime = 'text/x-mariadb'
+        let theme = 'mdn-like'//设置主题，不设置的会使用默认主题
+        let editor = CodeMirror.fromTextArea(this.$refs.mycode, {
+          mode: mime,//选择对应代码编辑器的语言，我这边选的是数据库，根据个人情况自行设置即可
+          indentWithTabs: true,
+          smartIndent: true,
+          lineNumbers: true,
+          matchBrackets: true,
+          theme: theme,
+          autofocus: true,
+          extraKeys: {'Ctrl': 'autocomplete'},//自定义快捷键
+          hintOptions: {//自定义提示选项
+            tables: {
+              users: ['name', 'score', 'birthDate'],
+              countries: ['name', 'population', 'size']
+            }
+          }
+        })
+        //代码自动提示功能，记住使用cursorActivity事件不要使用change事件，这是一个坑，那样页面直接会卡死
+        // editor.on('cursorActivity', function () {
+        //   editor.showHint()
+        // })
+        editor.on('change', function(){
+          global_varibles.sqlCode = editor.getValue()
+          console.log(global_varibles.sqlCode)
+        })
+      }
 	}
 </script>
 
 <style>
-
+  .codesql {
+    font-size: 18pt;
+    font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+  }
 </style>
---
+++ src/components/global_varibles.vue
@@ -0,0 +1,7 @@
+<script>
+  const sqlCode = '';
+
+  export default {
+    sqlCode
+  }
+</script>
